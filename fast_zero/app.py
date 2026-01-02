f3c7abaaeb1976ea4ef807e3108d97b495b3d723
Caio Hilário <caio.hilario@ufv.br>
2026-01-02 20:09:27 -0300

    Aula 2: add schemas e exercicio endpoint html

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,28 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from .schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+# Exercício da aula 2, fazer um endpoint que retorne um Olá mundo em HTML e
+# fazer o teste com o pytest
+@app.get('/exercicio2', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+def html_olamundo():
+    return """
+    <html>
+        <head>
+            <title> Olá mundo!</title>
+        </head>
+        <body>
+            <h1> Olá mundo! </h1>
+        </body>
+    </html>"""
--- tests/test_app.py
+++ tests/test_app.py
@@ -12,3 +12,13 @@ def test_root_deve_retornar_ok_e_ola_mundo():
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá Mundo!'}
+
+
+# Teste do exercicio da aula 2
+def test_exercicio2_deve_retornar_ok_e_html_escrito_ola_mundo():
+    client = TestClient(app)
+
+    response = client.get('/exercicio2')
+
+    assert response.status_code == HTTPStatus.OK
+    assert '<h1> Olá mundo! </h1>' in response.text
